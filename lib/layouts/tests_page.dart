a6f5610f8070b464135cf6d0c7a617be279e4974
Kreatbox <ammar.s.d.k.9@gmail.com>
2024-07-04 00:28:58 +0300

    "Refactored main.dart to remove demo code and add navigation to new pages; updated widget_test.dart to match changes"

---
+++ lib/layouts/tests_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tests'),
+      ),
+      body: Center(
+        child: Text('This is the tests page'),
+      ),
+    );
+  }
+}
