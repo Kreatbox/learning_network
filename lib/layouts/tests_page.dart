d582543dfed3debb919a182b58138351d64d1c8b
Kreatbox <ammar.s.d.k.9@gmail.com>
2024-07-04 01:47:03 +0300

    nothing

--- lib/layouts/tests_page.dart
+++ lib/layouts/tests_page.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class TestsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('التدريبات'),
+      ),
+      body: Center(
+        child: Text('هذه هي صفحة التدريبات'),
+      ),
+    );
+  }
+}
